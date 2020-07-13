262244ac0cd8a46561b4b237c83656decc0669d1
Behzad Khosravani Majd <68026879+bkm-999@users.noreply.github.com>
2020-07-13 14:42:35 +0430

    Update config.js

--- control/config.js
+++ control/config.js
@@ -1,4 +1,4 @@
-mongodbUrl = 'mongodb+srv://Admin:@Dm!n@p1.4jm6a.mongodb.net/p1?retryWrites=true&w=majority';
+mongodbUrl = 'mongodb+srv://<username>:<password>@p1.4jm6a.mongodb.net/p1?retryWrites=true&w=majority';
 
 module.exports = {
     mongoURI: mongodbUrl
